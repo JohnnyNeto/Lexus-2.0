807003c20ee9cfb6f00d2c3317c8a5d06cef36c9
JohnnyNeto <picmi320@gmail.com>
2025-05-13 14:33:50 -0300

    Atualiza código com novas mudanças

--- lexus frontend/js/script.js
+++ lexus frontend/js/script.js
@@ -11,3 +11,32 @@ document.querySelector('form').addEventListener('submit', function(e) {
     }
   });
   
+  // Dropdown Produzir já existente
+const btn = document.getElementById('produzir-btn');
+const dropdown = document.getElementById('produzir-dropdown');
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  dropdown.style.display = dropdown.style.display === 'flex' ? 'none' : 'flex';
+});
+
+document.addEventListener('click', function (e) {
+  if (!btn.contains(e.target) && !dropdown.contains(e.target)) {
+    dropdown.style.display = 'none';
+  }
+});
+
+// Dropdown Perfil
+const perfilBtn = document.getElementById('perfil-btn');
+const perfilDropdown = document.getElementById('perfil-dropdown');
+
+perfilBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  perfilDropdown.style.display = perfilDropdown.style.display === 'flex' ? 'none' : 'flex';
+});
+
+document.addEventListener('click', function (e) {
+  if (!perfilBtn.contains(e.target) && !perfilDropdown.contains(e.target)) {
+    perfilDropdown.style.display = 'none';
+  }
+});
